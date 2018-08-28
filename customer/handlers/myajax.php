4ce64a46ad6cb891c47a69334d8eaf49c5cd5a0c
ziovik <mondaydaniel2002@yahoo.com>
2018-08-28 15:55:07 +0300

    Merge branch 'master' of https://github.com/ziovik/optimum
    # Conflicts:
    #	inc/functions.php
    #	optimum_beauty.php

---
+++ customer/handlers/myajax.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: daniel
+ * Date: 8/28/2018
+ * Time: 1:18 PM
+ */
---
+++ db_objects/Message.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: daniel
+ * Date: 8/28/2018
+ * Time: 3:14 PM
+ */
+
+class message
+{
+
+}
